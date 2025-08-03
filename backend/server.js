91cea68092122cd8fbd07637099a39fcf9e36175
Priyank Adhav <priyankadhav03@gmail.com>
2025-08-03 18:17:02 +0530

    feat: add student routes and mount them

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,4 @@ app.get('/', (req, res) => res.json({ success: true, msg: 'Backend alive'}));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('Server running on ${PORT}'));
+app.use('/api/students', require('./routes/studentRoutes'));
