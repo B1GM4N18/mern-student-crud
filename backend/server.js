51d97aa32c0e66c1a40c6312decc9befa1a36d1f
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-08-03 19:33:47 +0530

    fix: enable CORS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const connectDB = require('./config/db');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
